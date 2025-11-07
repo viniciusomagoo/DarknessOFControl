4c839a5f1798a1765393c7760794252208042927
Vinícius, o Mago <vinicius.gomesz2025@gmail.com>
2025-11-07 10:45:59 -0300

    Código principal
    ashawot, osha

---
+++ public/Créditos/creditos.js
@@ -0,0 +1,9 @@
+// Função de placeholder para o botão "Ver Time"
+function showTeam(memberName) {
+    alert(`${memberName}: Desconhecida [DEBUGGING]`);
+}
+
+// Função de placeholder para o botão "Ver Detalhes"
+function showDetails(memberName) {
+    alert(`[DEBUG] Visualizando detalhes de: ${memberName}\n(Função ainda não implementada)`);
+}
